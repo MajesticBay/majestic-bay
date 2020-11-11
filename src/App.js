beb4184961232aed902b93f1b59b2749ae804df3
Sergey <in43sh@gmail.com>
2020-11-11 04:46:00 -0700

    Start working on main-content container; Add forest image.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Line from "./Line";
 import logo from "./images/majestic-bay.svg";
+// import macbook from "./images/macbook.png";
+import weCode from "./images/we-code.png";
 import whatWeDo from "./images/what-we-do.svg";
 // import ourTeam from "./images/our-team.svg";
 import "./scss/main.css";
@@ -12,7 +14,17 @@ function App() {
       </header>
       <main className="main">
         <div className="top-screen">
+          <div className="top-screen__macbook">
+            <img className="top-screen__we-code-img" src={weCode} alt="we code"/>
+          </div>
         </div>
+        {/* <div className="what-we-do">
+          <img className="what-we-do__img-header" src={whatWeDo} alt="what we do"/>
+          <p className="what-we-do__content">We successfully solve business problems through design and code</p>
+        </div>
+        <Line />
+        <Line color={"--dark"}/>
+        <Line /> */}
         <div className="what-we-do">
           <img className="what-we-do__img-header" src={whatWeDo} alt="what we do"/>
           <p className="what-we-do__content">We successfully solve business problems through design and code</p>
@@ -20,6 +32,11 @@ function App() {
         <Line />
         <Line color={"--dark"}/>
         <Line />
+        <div className="main-content">
+          <div className="main-content__bg"></div>
+          
+        </div>
+
       </main>
     </>
   );
