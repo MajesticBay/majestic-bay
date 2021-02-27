992d5d74dcc1941248d37e027b81060a95b4cef8
Sergey <in43sh@gmail.com>
2021-02-27 06:24:30 -0600

    Destructure props in a better way in Line.js file.

--- src/Line.js
+++ src/Line.js
@@ -1,19 +1,16 @@
-function Line(props) {
-    // let color = {props}
-    return (
-        // <div className={"line " + props.color ? "line" + props.color : ""}>
-        // <div className={props.color ? "line" + props.color : "" + " line"}>
-        // <div className={"line " + "line" + props.color}>
-        <div className={"line line" + props.color}>
-            <span className="line__text">AR Design</span>
-            <span className="line__text"><span className="line__text--bold">React</span> Native</span>
-            <span className="line__text">Efficient Code</span>
-            <span className="line__text">Backend</span>
-            <span className="line__text"><span className="line__text--bold">App</span>Design</span>
-            <span className="line__text"><span className="line__text--bold">Web</span>Design</span>
-            <span className="line__text"><span className="line__text--bold">UX</span> Research</span>
-        </div>
-    )
-}
+export const Line = ({color}) => (
+    // <div className={"line " + props.color ? "line" + props.color : ""}>
+    // <div className={props.color ? "line" + props.color : "" + " line"}>
+    // <div className={"line " + "line" + props.color}>
+    <div className={"line line" + color}>
+        <span className="line__text">AR Design</span>
+        <span className="line__text"><span className="line__text--bold">React</span> Native</span>
+        <span className="line__text">Efficient Code</span>
+        <span className="line__text">Backend</span>
+        <span className="line__text"><span className="line__text--bold">App</span>Design</span>
+        <span className="line__text"><span className="line__text--bold">Web</span>Design</span>
+        <span className="line__text"><span className="line__text--bold">UX</span> Research</span>
+    </div>
+)
 
 export default Line;
