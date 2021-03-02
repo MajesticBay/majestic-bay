8888b2a2b81b3c12ff0bf60bbdf0d318dfaf6ccf
Sergey <in43sh@gmail.com>
2021-03-02 06:53:36 -0600

    Create Banner component.

---
+++ src/components/Banner.js
@@ -0,0 +1,8 @@
+export const Banner = () => {
+    return (
+        <div className="banner">
+            <p>We always solve client's problem in the most optimal and effective way.</p>
+            <button>CONTACT US</button>
+        </div>
+    )
+}
