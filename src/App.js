72a5805b141570d81f0a6fc313df04dfd0ecfcbf
Sergey <in43sh@gmail.com>
2021-03-02 04:38:40 -0600

    Move 2 components inside the background container.

--- src/App.js
+++ src/App.js
@@ -26,13 +26,14 @@ function App() {
         <Line color={"--dark"}/>
         <Line />
         <div className="main-content">
-          <div className="main-content__bg"></div>
-          <div className="main-content__story">
-            <p>We are excellent experts. <span className="main-content__story--blue">Web design, UX research, app design, branding and logos, highly loaded backend, lightweight and responsive frontend</span> — we all know how to do it at the highest level.</p>
-            <br/>
-            <p>And most importantly — <span className="main-content__story--blue">we do not forget to solve the client's problem in the most optimal</span> and effective way.</p>
+          <div className="main-content__bg">
+            <div className="main-content__story">
+              <p>We are excellent experts. <span className="main-content__story--blue">Web design, UX research, app design, branding and logos, highly loaded backend, lightweight and responsive frontend</span> — we all know how to do it at the highest level.</p>
+              <br/>
+              <p>And most importantly — <span className="main-content__story--blue">we do not forget to solve the client's problem in the most optimal</span> and effective way.</p>
+            </div>
+            <OurWorks />
           </div>
-          <OurWorks />
           <OurTeam />
         </div>
       </main>
