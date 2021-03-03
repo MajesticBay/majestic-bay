0a8a2210937c6a9de3a201708c586e7939e09983
Sergey <in43sh@gmail.com>
2021-03-03 06:06:20 -0600

    Replace what we do header image with text.

--- src/components/WhatWeDo.js
+++ src/components/WhatWeDo.js
@@ -2,7 +2,8 @@ import whatWeDo from "../images/what-we-do.svg";
 
 export const WhatWeDo = () => (
     <div className="what-we-do">
-        <img className="what-we-do__img-header" src={whatWeDo} alt="what we do"/>
+        {/* <img className="what-we-do__img-header" src={whatWeDo} alt="what we do"/> */}
+        <h2 className="header">what<br/>we do?</h2>
         <p className="header-description">We successfully solve business problems through design and code</p>
     </div>
 )
