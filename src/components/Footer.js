c34880fac9217e7a70e8187b19b601358ba225c9
Sergey <in43sh@gmail.com>
2021-03-02 21:31:02 -0600

    Add Footer component.

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import logo from '../images/majestic-bay.svg';
+
+export const Footer = () => {
+    return (
+        <div>
+            <img src={logo} alt="logo" />
+            <p>© 2018–2021 Majestic Bay</p>
+        </div>
+    )
+}
