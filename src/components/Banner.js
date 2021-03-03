3b1ac1e61f705f5be28ad41870e481b966feea68
Sergey <in43sh@gmail.com>
2021-03-03 00:47:16 -0600

    Work on styling Banner component.

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,8 +3,8 @@ import { Btn } from './Btn';
 export const Banner = () => {
     return (
         <div className="banner">
-            <p>We always solve client's problem in the most optimal and effective way.</p>
-            <Btn dark={true}/>
+            <p className="banner__text">We always solve client's problem in the most optimal and effective way.</p>
+            <Btn className={"banner__btn"} dark={true}/>
         </div>
     )
 }
