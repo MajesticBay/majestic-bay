6a409acd7d0322259978bb73fdb5932a908cf104
Sergey <in43sh@gmail.com>
2021-03-03 04:15:14 -0600

    Work on styling for OurTeam: add markup for a single team member, start working on top info for a team member.

--- src/components/OurTeam.js
+++ src/components/OurTeam.js
@@ -1,6 +1,19 @@
+import mike from '../images/mike.png';
+
 export const OurTeam = () => (
     <div className="our-team">
         <h1 className="header">our<br/>team</h1>
         <p className="header-description">We are excellent experts. We are excellent experts. We are excellent experts.</p>
+        <div className="our-team__members-container">
+            <div className="member">
+                <div className="member__top">
+                    <img className="member__emoji" src={mike} alt="Mike emoji"/>
+                    <h3 className="member__name">Mike</h3>
+                    <p className="member__title">APP / UX / AR & Product Designer</p>
+                </div>
+                <p><span>From Mike:</span>For more than 7 years I have been helping brands develop their business through design. Visual support of the brand — my task. I like to work on projects from the start and use all my experience to do the job perfectly. </p>
+                <a href="#">contact Mike</a>
+            </div>
+        </div>
     </div>
 )
