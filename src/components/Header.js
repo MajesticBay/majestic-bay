9f92e9ddf98c9cfbbff20a5c7c82f3de05e6402d
Sergey <in43sh@gmail.com>
2021-03-04 00:06:07 -0600

    Add contact-us link to the header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,9 @@ import logo from "../images/majestic-bay.svg";
 
 export const Header = () => (
     <header className="header-container">
-        <img src={logo} alt="logo"/>
+        <a className="header-container__link">
+            <img className="header-container__logo" src={logo} alt="logo"/>
+            <p className="header-container__contact-us">contact us</p>
+        </a>
     </header>
 )
