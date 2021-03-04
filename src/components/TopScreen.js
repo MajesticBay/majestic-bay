e110abe70b8e4bfdd69fa24f29b7b57b8d7c7b6c
Sergey <in43sh@gmail.com>
2021-03-03 23:58:44 -0600

    Work on TopScreen.

--- src/components/TopScreen.js
+++ src/components/TopScreen.js
@@ -1,10 +1,12 @@
 import weCode from "../images/we-code.png";
+import weCodeMobile from "../images/we-code-mobile.svg";
 // import macbook from "./images/macbook.png";
 // import ourTeam from "./images/our-team.svg";
 
 export const TopScreen = () => (
     <div className="top-screen">
         <div className="top-screen__macbook">
+            <img className="top-screen__we-code-img-mobile" src={weCodeMobile} alt="we code"/>
             <img className="top-screen__we-code-img" src={weCode} alt="we code"/>
             {/* <p className="top-screen__contact-us">contact us</p> */}
         </div>
