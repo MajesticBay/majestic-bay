fc7854e93ef9eb7c428a919e8e933fcff8b30e04
Sergey <in43sh@gmail.com>
2021-03-06 00:19:11 -0600

    Bring in new version of the TopScreen.

--- src/components/TopScreen.js
+++ src/components/TopScreen.js
@@ -1,13 +1,34 @@
+import bg from "../images/bg.png";
+import mac from "../images/macbook.png";
+import iphone from "../images/iphone.png";
 import weCode from "../images/we-code.png";
-import weCodeMobile from "../images/we-code-mobile.svg";
-// import macbook from "./images/macbook.png";
-// import ourTeam from "./images/our-team.svg";
+import weCodeMobile from "../images/wecode-mobile.png";
 
-export const TopScreen = () => (
-    <div className="top-screen">
-        <div className="top-screen__macbook">
-            <img className="top-screen__we-code-img-mobile" src={weCodeMobile} alt="we code"/>
-            <img className="top-screen__we-code-img" src={weCode} alt="we code"/>
+export const TopScreen = () => {
+  const bgStyle = {
+    backgroundImage: `url(${bg})`,
+  };
+  return (
+    <div className="app-container" style={bgStyle}>
+      <div className="macbook-screen">
+        <img src={mac} alt="" className="desktop" />
+        <img src={iphone} alt="" className="mobile" />
+        <div className="wecode-img-wrap">
+          <img src={weCode} alt="" className="we-code-desktop" />
+          <img src={weCodeMobile} alt="" className="we-code-mobile" />
         </div>
+      </div>
+      <div className="wecode-wrap">
+        <div className="wecode-text">
+          <h1>
+            what <br /> we do?
+          </h1>
+          <p>
+            We successfully solve <br /> business problems through <br /> design
+            and code
+          </p>
+        </div>
+      </div>
     </div>
-)
+  );
+};
