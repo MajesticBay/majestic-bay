6a698f6bd15c0c7346b7693f69710181cbfbef9c
Joe Siburov <g.siburov@gmail.com>
2022-02-03 17:46:43 +0200

    states cleaned

--- src/components/MovingLines.js
+++ src/components/MovingLines.js
@@ -1,36 +1,12 @@
 import { Line } from "./Line";
-import { useState, useEffect } from "react";
-
 
 export const MovingLines = () => {
-  const [count, setCount] = useState(0);
-  const [outernLine, setOuternLine] = useState("left");
-  const [innerLine, setInnerLine] = useState("right");
-
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-       setCount(count + 1);
-     }, 5000);
- 
-    if (outernLine === "left") {
-      setOuternLine("right");
-    } else {
-      setOuternLine("left");
-    }
-
-    if (innerLine === "left") {
-      setInnerLine("right");
-    } else {
-      setInnerLine("left");
-    }
-    return () => clearTimeout(timeout);
-   },[count]);
 
   return (
     <>
-      <Line turnSpeed={0.25} direction={outernLine}/>
-      <Line turnSpeed={0.1} direction={innerLine} color={"--dark"} />
-      <Line turnSpeed={0.05} direction={outernLine}/>
+      <Line turnSpeed={0.25} direction={"left"}/>
+      <Line turnSpeed={0.1} direction={"right"} color={"--dark"} />
+      <Line turnSpeed={0.05} direction={"left"}/>
     </>
   );
 };
