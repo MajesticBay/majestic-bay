d292a195d71a36fb377a72ea2ce00dd8eb5b5408
Sergey Sherstobitov <in43sh@gmail.com>
2022-02-16 21:18:28 -0800

    Make TeamMember more dynamic.

--- src/components/TeamMember.js
+++ src/components/TeamMember.js
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import styled from 'styled-components';
-import mike from '../images/mike.png';
 
 const About = styled.p`
     display: ${({ about }) => (about) ? "block" : "none"};
@@ -20,15 +19,15 @@ const QuoteAbout = styled.p`
     color: var(--color-secondary);
 `;
 
-export const TeamMember = ({name}) => {
+export const TeamMember = ({person}) => {
     const [about, setAbout] = useState(true);
 
     return (
         <div className="member">
             <div className="member__top">
-                <img className="member__emoji" src={mike} alt="Mike emoji" />
-                <h3 className="member__name">{name}</h3>
-                <p className="member__title">APP / UX / AR & Product Designer</p>
+                <img className="member__emoji" src={person.img} alt="Team member emoji" />
+                <h3 className="member__name">{person.name}</h3>
+                <p className="member__title">{person.position}</p>
             </div>
             {/* <p className="member__about"><span className="green">From Mike:</span>{'\u00A0'}For more than 7 years I have been helping brands develop their business through design.{'\u00A0'}<span className="italic">Visual support of the brand — my task.</span>{'\u00A0'}I like to work on projects from the start and use all my experience to do the job perfectly.</p> */}
             <About about={about} onMouseEnter={() => setAbout(!about)}>
@@ -37,7 +36,8 @@ export const TeamMember = ({name}) => {
             </About>
             {/* <p className="member__quote-about">Mike — is the best designer on both sides of the ocean :)</p> */}
             <QuoteAbout about={about} onMouseLeave={() => setAbout(!about)}>
-                Mike — is the best designer on both sides of the ocean (smiley face)
+                {/* Mike — is the best designer on both sides of the ocean (smiley face) */}
+                { person.quoteAbout }
             </QuoteAbout>
             <a className="member__contact" href="http://localhost:3000/">contact Mike</a>
         </div>
