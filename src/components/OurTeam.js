67a0771dc46e5cf50f5af0e7e86cb2c5819db5b1
Sergey Sherstobitov <in43sh@gmail.com>
2022-02-23 12:09:29 -0800

    Fix whitespaces in team members positions titles; updte some team members positions titles.

--- src/components/OurTeam.js
+++ src/components/OurTeam.js
@@ -12,21 +12,21 @@ export const OurTeam = () => {
         },
         {
             name: "Sergey",
-            position: "Fullstack Developer",
+            position: "Front+Back End",
             about: "I love breaking the rules and putting them back together in my way to create attractive, innovative designs. I also love creating beautiful and user-friendly sites, creating the perfect balance of form and function.",
             quoteAbout: "Sergey is a developer with a human soul.",
             img: mike
         },
         {
             name: "Heorhii",
-            position: "Front+Back End/DB/Cloud/DevOps/ML",
+            position: "Front+Back End / DB / Cloud / DevOps / ML",
             about: "There are no unsolvable tasks in this world. If there is at least one - we will find the magic to make it solvable. I adore building any kind of architecture - from tiny one-page applications to enormous scalable online services. I like & ready to develop and move your recourse to the cloud right away. I teach kids how to install Linux.",
             quoteAbout: "Joe - that guy from your side of the ocean who knows how to build and deliver better.",
             img: mike
         },
         {
             name: "Liliya",
-            position: "Fullstack Developer",
+            position: "Front+Back End",
             about: "When you find an idea that you just can't stop thinking about, that's probably a good one to pursue. Some of the world's most remarkable ideas have started with just a nagging feeling. I am the one who does the actual implementation of the existing idea.",
             quoteAbout: "Your success always inspires me.",
             img: mike
