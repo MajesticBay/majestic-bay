1b9a2025513c59f4cf67aff08940d538d3f27393
Sergey Sherstobitov <in43sh@gmail.com>
2022-02-23 13:24:24 -0800

    Abstract Projects content to a separate component.

--- src/components/OurWorks.js
+++ src/components/OurWorks.js
@@ -4,6 +4,44 @@ import rpacenter from "../images/rpa.svg";
 import rseven from "../images/r-seven.svg";
 import designinvision from "../images/design-invision.svg";
 import homebuzz from "../images/homebuzz.svg";
+import { Project } from "./Project";
+
+const projectsData = [
+  {
+    name: "safe lit",
+    description: "Application, wich designed for people with spinal cord injuries, hernia or other problems that limit physical activity.",
+    img: savefit,
+  },
+  {
+    name: "leaf's deli",
+    description: "Web site and identity for the most enjoyable private café in the Seattle Industrial Zone.",
+    img: leafsdeli,
+  },
+  {
+    name: "rpa.center",
+    description: "Web site for company that develops and implements robotics process automatization based solutions.",
+    img: rpacenter,
+  },
+  {
+    name: "r-seven",
+    description: "Corporate identity and website for IT company developing a complex and highly loaded backend for government systems.",
+    img: rseven,
+  },
+  {
+    name: "design invision",
+    description: "Logo, animation and design for designer’s YouTube channel about lifehacks from the design.",
+    img: designinvision,
+  },
+  {
+    name: "homebuzz",
+    description: "Corporate style and template of an online store for a construction company or a spare parts store.",
+    img: homebuzz,
+  }
+]
+
+const projects = projectsData.map((project) => (
+  <Project project={project} />
+))
 
 export const OurWorks = () => (
   <div className="our-works">
@@ -16,53 +54,6 @@ export const OurWorks = () => (
       We do many projects and every job we do is a pride for us and our
       customers
     </p>
-    <div className="project">
-      <img src={savefit} alt="save fit app" />
-      <p className="project__header">safe lit</p>
-      <p className="project__description">
-        Application, wich designed for people with spinal cord injuries, hernia
-        or other problems that limit physical activity.
-      </p>
-    </div>
-    <div className="project">
-      <img src={leafsdeli} alt="leaf's deli website" />
-      <p className="project__header">leaf's deli</p>
-      <p className="project__description">
-        Web site and identity for the most enjoyable private café in the Seattle
-        Industrial Zone.
-      </p>
-    </div>
-    <div className="project">
-      <img src={rpacenter} alt="rpa.center website" />
-      <p className="project__header">rpa.center</p>
-      <p className="project__description">
-        Web site for company that develops and implements robotics process
-        automatization based solutions.
-      </p>
-    </div>
-    <div className="project">
-      <img src={rseven} alt="r-seven website" />
-      <p className="project__header">r-seven</p>
-      <p className="project__description">
-        Corporate identity and website for IT company developing a complex and
-        highly loaded backend for government systems.
-      </p>
-    </div>
-    <div className="project">
-      <img src={designinvision} alt="design invision website" />
-      <p className="project__header">design invision</p>
-      <p className="project__description">
-        Logo, animation and design for designer’s YouTube channel about
-        lifehacks from the design.
-      </p>
-    </div>
-    <div className="project">
-      <img src={homebuzz} alt="homebuzz website" />
-      <p className="project__header">homebuzz</p>
-      <p className="project__description">
-        Corporate style and template of an online store for a construction
-        company or a spare parts store.
-      </p>
-    </div>
+    { projects }
   </div>
 );
