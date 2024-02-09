7c6ea29399f10e538b79b7fcdbbaaca7f8762835
Joe Siburov <g.siburov@gmail.com>
2024-02-09 23:21:03 +0400

    ts easlin preparations are done

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,20 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root')!
-)
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+// Attempt to get the root element in a safe manner
+const rootElement = document.getElementById('root')
+if (rootElement !== null) {
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+  // If you want to start measuring performance in your app, pass a function
+  // to log results (for example: reportWebVitals(console.log))
+  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+  reportWebVitals()
+} else {
+  console.error('Failed to find the root element')
+}
--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,13 +1,15 @@
 import { type ReportHandler } from 'web-vitals'
 
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+const reportWebVitals = (onPerfEntry?: ReportHandler): void => {
+  if (onPerfEntry !== null && onPerfEntry !== undefined && typeof onPerfEntry === 'function') {
+    void import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry)
       getFID(onPerfEntry)
       getFCP(onPerfEntry)
       getLCP(onPerfEntry)
       getTTFB(onPerfEntry)
+    }).catch((error) => {
+      console.error('Failed to load web vitals', error)
     })
   }
 }
