a83c403e1b4377daeb8259880b476a7dfa0f2759
Joe Siburov <g.siburov@gmail.com>
2024-09-26 06:49:01 +0400

    app reset

--- src/components/WhatWeDo.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const WhatWeDo: React.FC = () => (
-    <div className="what-we-do">
-        {/* <img className="what-we-do__img-header" src={whatWeDo} alt="what we do"/> */}
-        <h2 className="header">what<br/>we do?</h2>
-        <p className="header-description">We successfully solve business problems through design and code</p>
-    </div>
-)
